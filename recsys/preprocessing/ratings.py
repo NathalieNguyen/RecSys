7d13f7da0460de384b0829b7e2adb5d26a8245d5
Nathalie Nguyen <nathalie.nguyen85@googlemail.com>
2018-10-08 10:44:20 +0200

    restructure directories and import own modules in jupyter

--- recsys/preprocessing/ratings.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-def load_raw_df():
-    return pd.read_csv('../data/BX-CSV-Dump/BX-Book-Ratings.csv',
-                       sep=';', error_bad_lines=False, encoding='latin-1', low_memory=False)
