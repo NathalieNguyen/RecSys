46e5fd4f099845b115f6cd6894eb05922eab027b
Nathalie Nguyen <nathalie.nguyen85@googlemail.com>
2018-10-17 10:44:08 +0200

    add knn without surprise package

--- collaborative_filtering_knn.py
+++ collaborative_filtering_knn.py
@@ -1,6 +1,4 @@
 from lib.preprocessing import data_for_training as data
-from surprise import KNNBasic
-from lib.evaluation import metrics as metrics
 from lib.evaluation import recommendations as recs
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -11,35 +9,26 @@ from scipy.sparse import csr_matrix
 if __name__ == '__main__':
     np.random.seed(0)
 
-    stratify_df = data.filter_ratings_above_count_threshold(sample=20000)
-
-    # Choose either densified ratings df or sample for building the train set
+    stratify_df = data.filter_ratings_above_count_threshold(frac=0.1)
     train_set, test_set = train_test_split(stratify_df, stratify=stratify_df['User-ID'])
 
-    IxU_train = csr_matrix(train_set.pivot(index='ISBN', columns='User-ID', values='Book-Rating').fillna(0))
-    IxU_test = csr_matrix(test_set.pivot(index='ISBN', columns='User-ID', values='Book-Rating').fillna(0))
+    IxU_train = train_set.pivot(index='ISBN', columns='User-ID', values='Book-Rating').fillna(0)
+    IxU_train_sparse = csr_matrix(train_set.pivot(index='ISBN', columns='User-ID', values='Book-Rating').fillna(0))
+    IxU_test_sparse = csr_matrix(test_set.pivot(index='ISBN', columns='User-ID', values='Book-Rating').fillna(0))
 
-    print(IxU_train)
-    print(IxU_test)
+    item_knn = NearestNeighbors(metric='cosine', algorithm='brute')
+    item_knn.fit(IxU_train_sparse)
 
-    # item_knn = NearestNeighbors(metric='cosine', algorithm='brute')
-    # item_knn.fit(IxU_train)
+    query_index = np.random.choice(IxU_train.shape[0])
+    distances, indices = item_knn.kneighbors(IxU_train.iloc[query_index, :].values.reshape(1, -1), n_neighbors=10)
 
-    # Training and predictions based on full data set with anti test set for building recommendations
-    # full_train_set = data.load_full_ratings_train_set(ratings_df_param_densified)
-    #
-    # item_knn = KNNBasic(k=10, sim_options={'name': 'pearson_baseline', 'user_based': False})
-    # item_knn.fit(full_train_set)
-    #
-    # anti_test_set = full_train_set.build_anti_testset()
-    # recs_pred = item_knn.test(anti_test_set)
-    #
-    # print(recs.show_recommendations(recs_pred=recs_pred, user_id=276994))
+    print(distances)
+    print(indices)
 
-    # Training and predictions based on split data set for evaluating metrics
-    # item_knn.fit(IxU_train)
-    # test_pred = item_knn.test(IxU_test)
+    isbn_book_dict = recs.map_isbn_to_names()
 
-    # print("Item-based collaborative filtering with KNN: Test set")
-    # print('MAE: ', metrics.MAE(test_pred))
-    # print('RMSE: ', metrics.RMSE(test_pred))
+    print('Recommendations for {0}, ISBN: {1}:\n'.format(isbn_book_dict[IxU_train.index[query_index]], IxU_train.index[query_index]))
+
+    for i in range(0, len(distances.flatten())):
+        print('{0}: {1}, ISBN: {2} with distance of {3}:'.format(i, isbn_book_dict[IxU_train.index[indices.flatten()[i]]],
+                                                                 IxU_train.index[indices.flatten()[i]], distances.flatten()[i]))
